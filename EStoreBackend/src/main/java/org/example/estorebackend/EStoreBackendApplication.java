bde2c294c1d14971898771690a77b5af65058758
sourabha s mohanty <sourabhasikun20@gmail.com>
2023-12-12 01:02:53 +0530

    First Commit

---
+++ EStoreBackend/src/main/java/org/example/estorebackend/EStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.estorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EStoreBackendApplication.class, args);
+    }
+
+}
--- EStoreBackend/src/test/java/org/example/estorebackend/EStoreBackendApplicationTests.java
+++ EStoreBackend/src/test/java/org/example/estorebackend/EStoreBackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.esotrebackend;
+package org.example.estorebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EsotreBackendApplicationTests {
+class EStoreBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
