5d8df05393517a13d37389421c414c89cf9ad30f
sourabha s mohanty <sourabhasikun20@gmail.com>
2023-12-12 00:12:24 +0530

    First Commit

---
+++ EsotreBackend/src/main/java/com/example/esotrebackend/EsotreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.esotrebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsotreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsotreBackendApplication.class, args);
+	}
+
+}
---
+++ EsotreBackend/src/test/java/com/example/esotrebackend/EsotreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esotrebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsotreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
