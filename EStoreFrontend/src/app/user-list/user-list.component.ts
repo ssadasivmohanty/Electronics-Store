d735dc01514a611dfcdfafc4586ced690cc065f1
sourabha s mohanty <sourabhasikun20@gmail.com>
2023-12-14 01:20:05 +0530

    user-list working

---
+++ EStoreFrontend/src/app/user-list/user-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+  users: any[] = [];
+  constructor(private apiService : ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getUsers().subscribe(
+      (data: any[]) => {
+        this.users = data;
+        console.log(data);
+      },
+      (error) => {
+        console.error('Error fetching users:', error);
+      }
+    );
+  }
+
+}
