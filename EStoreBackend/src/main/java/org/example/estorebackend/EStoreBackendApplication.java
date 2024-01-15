3533fc03b7a71d1f9201d86cd9fd1ec8f348ddb0
ssadasivmohanty <sourabhasikun20@gmail.com>
2024-01-15 14:58:32 +0530

    changes made by souravapsys

--- EStoreBackend/src/main/java/org/example/estorebackend/EStoreBackendApplication.java
+++ EStoreBackend/src/main/java/org/example/estorebackend/EStoreBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EStoreBackendApplication {
-    //This is a change made by souravapsys
     public static void main(String[] args) {
         SpringApplication.run(EStoreBackendApplication.class, args);
     }
