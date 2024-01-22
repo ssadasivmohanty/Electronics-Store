4077b0f8613f74a6e2834fcddfa9467dbb9168e7
sourabha s mohanty <sourabhasikun20@gmail.com>
2024-01-23 00:57:18 +0530

    added product-add component

--- EStoreFrontend/src/app/app.module.ts
+++ EStoreFrontend/src/app/app.module.ts
@@ -13,12 +13,11 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { UserListComponent } from './pages/user-list/user-list.component';
 import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './pages/register/register.component';
-import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
-import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 import { NavbarComponent } from './pages/navbar/navbar.component';
 import { AboutComponent } from './pages/about/about.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { UserProfilePopupComponent } from './user-profile-popup/user-profile-popup.component';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
 
 @NgModule({
   declarations: [
@@ -30,12 +29,11 @@ import { UserProfilePopupComponent } from './user-profile-popup/user-profile-pop
     LandingComponent,
     UserListComponent,
     RegisterComponent,
-    AdminDashboardComponent,
-    UserDashboardComponent,
     NavbarComponent,
     AboutComponent,
     FooterComponent,
-    UserProfilePopupComponent
+    UserProfilePopupComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
