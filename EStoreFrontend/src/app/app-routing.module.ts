6a4b20d49b7c3bec5c837893ddeaa346082bc471
sourabha s mohanty <sourabhasikun20@gmail.com>
2024-01-23 01:03:00 +0530

    Revert "added product-add component"
    This reverts commit 4077b0f8613f74a6e2834fcddfa9467dbb9168e7.

--- EStoreFrontend/src/app/app-routing.module.ts
+++ EStoreFrontend/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { UserListComponent } from './pages/user-list/user-list.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AboutComponent } from './pages/about/about.component';
 import { UserProfilePopupComponent } from './user-profile-popup/user-profile-popup.component';
@@ -24,6 +26,9 @@ const routes: Routes = [
   },
   {
     path:'dashboard', component:DashboardComponent,
+  },
+  {
+    path:'admin-dashboard', component:AdminDashboardComponent,
   },
   {
     path:'about', component:AboutComponent,
--- EStoreFrontend/src/app/app.module.ts
+++ EStoreFrontend/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { UserListComponent } from './pages/user-list/user-list.component';
 import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './pages/register/register.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 import { NavbarComponent } from './pages/navbar/navbar.component';
 import { AboutComponent } from './pages/about/about.component';
 import { FooterComponent } from './pages/footer/footer.component';
@@ -28,6 +30,8 @@ import { UserProfilePopupComponent } from './user-profile-popup/user-profile-pop
     LandingComponent,
     UserListComponent,
     RegisterComponent,
+    AdminDashboardComponent,
+    UserDashboardComponent,
     NavbarComponent,
     AboutComponent,
     FooterComponent,
